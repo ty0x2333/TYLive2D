dbdb9b279fd08300e060af4985c6d2749e1b98aa
luckytianyiyan <luckytianyiyan@gmail.com>
2017-02-26 17:17:44 +0800

    M TYLive2DModel

--- TYLive2D/Classes/TYLive2DModel.m
+++ TYLive2D/Classes/TYLive2DModel.m
@@ -8,6 +8,11 @@
 
 #import "TYLive2DModel.h"
 
+static NSString * const TYLive2DModelTexturesKey = @"ModelTextures";
+static NSString * const TYLive2DModelNameKey = @"ModelName";
+static NSString * const TYLive2DModelParamKey = @"ModelParam";
+static NSString * const TYLive2DModelPartKey = @"ModelPart";
+
 @implementation TYLive2DModel
 
 - (instancetype)initWithPlistPath:(NSString *)plistPath {
@@ -16,15 +21,15 @@
         NSDictionary *infos = [NSDictionary dictionaryWithContentsOfFile:plistPath];
         NSString *basePath = [plistPath stringByReplacingOccurrencesOfString:plistPath.lastPathComponent withString:@""];
         NSMutableArray *texturePaths = [NSMutableArray arrayWithCapacity:0];
-        for (NSString *texture in infos[@"ModelTextures"]) {
+        for (NSString *texture in infos[TYLive2DModelTexturesKey]) {
             [texturePaths addObject:[basePath stringByAppendingPathComponent:texture]];
         }
         _texturePaths = [texturePaths copy];
         
-        _modelPath = [basePath stringByAppendingPathComponent:infos[@"ModelName"]];
+        _modelPath = [basePath stringByAppendingPathComponent:infos[TYLive2DModelNameKey]];
         
-        _parameters = [infos[@"ModelParam"] allKeys];
-        _parts = infos[@"ModelPart"];
+        _parameters = [infos[TYLive2DModelParamKey] allKeys];
+        _parts = infos[TYLive2DModelPartKey];
     }
     return self;
 }
