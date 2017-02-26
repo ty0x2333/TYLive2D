3dca63af4a16769131b9a595cee2e79ef89b4441
luckytianyiyan <luckytianyiyan@gmail.com>
2017-02-26 15:39:34 +0800

    M Example

--- Example/TYLive2D/TYViewController.m
+++ Example/TYLive2D/TYViewController.m
@@ -7,23 +7,29 @@
 //
 
 #import "TYViewController.h"
+#import <TYLive2D/TYLive2DModel.h>
+#import <TYLive2D/TYLive2DView.h>
 
 @interface TYViewController ()
+
+@property (nonatomic, strong) TYLive2DView *live2DView;
 
 @end
 
 @implementation TYViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    TYLive2DModel *model = [[TYLive2DModel alloc] initWithPlistPath:[[NSBundle mainBundle] pathForResource:@"model" ofType:@"plist" inDirectory:@"Haru"]];
+    _live2DView = [[TYLive2DView alloc] init];
+    [_live2DView loadModel:model];
+    [self.view addSubview:_live2DView];
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    _live2DView.frame = self.view.bounds;
 }
 
 @end
